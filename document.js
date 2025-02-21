2114abc4a8d6ef6cfe30143145e9976d04055c6a
Anaëlle <anaellewiersch@gmail.com>
2025-02-21 22:18:55 +0100

    Update

--- document.js
+++ document.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Le document est prêt.");
 });
+
+
+let visitCount = localStorage.getItem("visitCount");
+
+if (visitCount === null) {
+    visitCount = 1; // Première visite
+} else {
+    visitCount = parseInt(visitCount) + 1; // Augmenter le compteur
+}
+
+// Mettre à jour le stockage local
+localStorage.setItem("visitCount", visitCount);
+
+// Afficher le nombre de visites
+document.getElementById("visitCount").textContent = visitCount;
